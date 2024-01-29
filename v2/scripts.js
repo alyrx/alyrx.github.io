da7b89c54fa44f5a366f4a6bf7ee4f7e58f5396c
alyrx <diogofino.df@gmail.com>
2024-01-29 16:53:19 +0000

    v2 homepage (WIP)

---
+++ v2/scripts.js
@@ -0,0 +1,29 @@
+function openDropdown() {
+    document.getElementById('headerDropdown').classList.toggle("hidden")
+}
+  
+function calculateContentHeight() {
+    var headerHeight = document.getElementById("header").offsetHeight;
+    var footerHeight = document.getElementById("footer").offsetHeight;
+    const windowHeight = window.innerHeight;
+
+    var contentHeight = ((windowHeight - (headerHeight + footerHeight)) / windowHeight) * 100;
+    
+    // alert(document.getElementById('content').classList[1]);
+    if (window.onload) {
+        document.getElementById('content').classList.add('flex');
+        document.getElementById('content').classList.add(`h-[${contentHeight}vh]`);
+    }
+
+    var pastContentHeight = document.getElementById('content').classList[1];
+    document.getElementById('content').classList.remove(pastContentHeight);
+    document.getElementById('content').classList.add(`h-[${contentHeight}vh]`);
+}
+  
+window.onload = function () {
+    calculateContentHeight();
+}
+
+window.onresize = function () {
+    calculateContentHeight();
+}
