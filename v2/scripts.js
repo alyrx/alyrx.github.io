29c9d13f5d31c077a0cad916c89a7b0ad7af7ae5
alyrx <diogofino.df@gmail.com>
2024-02-14 01:29:19 +0000

    Alyrx webpage v2 - fix initial page & header

--- v2/scripts.js
+++ v2/scripts.js
@@ -1,30 +1,3 @@
 function openDropdown() {
     document.getElementById('headerDropdown').classList.toggle("hidden")
 }
-  
-function calculateContentHeight() {
-    var headerHeight = document.getElementById("header").offsetHeight;
-    var footerHeight = document.getElementById("footer").offsetHeight;
-    const windowHeight = window.innerHeight;
-
-    var contentHeight = ((windowHeight - (headerHeight + footerHeight)) / windowHeight) * 100;
-    
-    if (window.onload) {
-        document.getElementById('content').classList.add('flex');
-        document.getElementById('content').classList.add(`h-[${contentHeight}vh]`);
-    }
-
-    var pastContentHeight = document.getElementById('content').classList[1];
-    document.getElementById('content').classList.remove(pastContentHeight);
-    document.getElementById('content').classList.add(`h-[${contentHeight}vh]`);
-}
-  
-window.onload = function () {
-    calculateContentHeight();
-}
-
-window.onresize = function () {
-    if (window.innerWidth >= 768) {
-        calculateContentHeight();
-    }
-}
